2d97bb7fd63a0b9f329b76a8aa9fd8db8ea63caa
Omar <95493823+omarkhaleddd@users.noreply.github.com>
2023-12-05 19:28:04 +0200

    backend files

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+// getting-started.js
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
