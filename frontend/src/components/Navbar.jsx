2cc05d79816bde70282827e99457d9b9dcc3e2e2
Omar <95493823+omarkhaleddd@users.noreply.github.com>
2023-12-07 17:58:18 +0200

    updated navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,12 +3,14 @@ import { faArrowRightToBracket, faUserPlus, faCartShopping } from "@fortawesome/
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { openCartBar } from '../redux/cart/cartSlice'
+import { useState } from 'react'
 
 const Navbar = () => {
 
     const products = useSelector((state) => state.products)
     const cart = useSelector((state) => state.cartState.value)
     console.log(cart);
+    const [openNavbar , setOpenNavbar] = useState(false)
     const dispatch = useDispatch();
     const handleCartBar = () => {
         dispatch(openCartBar(true));
@@ -18,10 +20,10 @@ const Navbar = () => {
         <nav className="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm" >
             <div className="container">
                 <Link to={"/"} className="navbar-brand fw-bold fs-4">Abo3mo Collection</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" onClick={()=>{setOpenNavbar(!openNavbar)}} data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <div className={`collapse navbar-collapse ${openNavbar?`show text-center`:``} `} id="navbarSupportedContent">
                     <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
                         <li className="nav-item">
                             <Link to={"/"} className="nav-link " aria-current="page">Home</Link>
